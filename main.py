7f3895b961adafb6ddd43a34e7321bcad2e97352
albsana <117643415+albsana@users.noreply.github.com>
2024-01-05 12:56:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,3 @@ if __name__ == '__main__':
     fr = naranjasFrameImpl(None)
     fr.Show()
     app.MainLoop()
-
-    #TEMP
-    ####		self.confButtonSpinCtrl1 = wx.SpinCtrlDouble( sbSizer3.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0.0, 10, 0 )
-    ####		self.confButtonSpinCtrl1.SetIncrement(float(0.01))
